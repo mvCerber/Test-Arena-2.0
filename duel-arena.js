e3d867acbe7c24f790440e7d3175840b9b6e7c18
mvCerber <cerber.mv@gmail.com>
2025-04-12 22:21:06 +0300

    Add files via upload

--- duel-arena.js
+++ duel-arena.js
@@ -1,13 +1,68 @@
-
 const contractAddress = "0x898D5b5F5ef690959dF87399972Db66308D3c02D";
-const abi = [/* ABI вставляється вручну на сайті або через окремий файл */];
+
+const abi = [
+  {
+    "inputs": [{"internalType": "uint64", "name": "subscriptionId", "type": "uint64"}],
+    "stateMutability": "nonpayable",
+    "type": "constructor"
+  },
+  {
+    "inputs": [],
+    "name": "joinAsPlayer1",
+    "outputs": [],
+    "stateMutability": "payable",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "joinAsPlayer2",
+    "outputs": [],
+    "stateMutability": "payable",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "reset",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "player1",
+    "outputs": [{"internalType": "address", "name": "", "type": "address"}],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "player2",
+    "outputs": [{"internalType": "address", "name": "", "type": "address"}],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "winner",
+    "outputs": [{"internalType": "address", "name": "", "type": "address"}],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "state",
+    "outputs": [{"internalType": "uint8", "name": "", "type": "uint8"}],
+    "stateMutability": "view",
+    "type": "function"
+  }
+];
 
 let provider;
 let signer;
 let contract;
 
 async function connectWallet() {
-    if (window.ethereum) {
+    if (typeof window.ethereum !== "undefined") {
         provider = new ethers.providers.Web3Provider(window.ethereum);
         await provider.send("eth_requestAccounts", []);
         signer = provider.getSigner();
@@ -20,32 +75,50 @@ async function connectWallet() {
 }
 
 async function joinAsPlayer1() {
-    const tx = await contract.joinAsPlayer1({ value: ethers.utils.parseEther("0.02") });
-    await tx.wait();
-    updateStatus();
+    try {
+        const tx = await contract.joinAsPlayer1({ value: ethers.utils.parseEther("0.02") });
+        await tx.wait();
+        updateStatus();
+    } catch (error) {
+        console.error(error);
+    }
 }
 
 async function joinAsPlayer2() {
-    const tx = await contract.joinAsPlayer2({ value: ethers.utils.parseEther("0.02") });
-    await tx.wait();
-    updateStatus();
+    try {
+        const tx = await contract.joinAsPlayer2({ value: ethers.utils.parseEther("0.02") });
+        await tx.wait();
+        updateStatus();
+    } catch (error) {
+        console.error(error);
+    }
 }
 
 async function resetDuel() {
-    const tx = await contract.reset();
-    await tx.wait();
-    updateStatus();
+    try {
+        const tx = await contract.reset();
+        await tx.wait();
+        updateStatus();
+    } catch (error) {
+        console.error(error);
+    }
 }
 
 async function updateStatus() {
-    const player1 = await contract.player1();
-    const player2 = await contract.player2();
-    const winner = await contract.winner();
-    const state = await contract.state();
-    document.getElementById("status").innerText = `
-        Player 1: ${player1}
-        Player 2: ${player2}
-        Winner: ${winner}
-        State: ${["WaitingForPlayer1","WaitingForPlayer2","InProgress","Finished"][state]}
-    `;
+    try {
+        const p1 = await contract.player1();
+        const p2 = await contract.player2();
+        const win = await contract.winner();
+        const state = await contract.state();
+
+        const states = ["WaitingForPlayer1", "WaitingForPlayer2", "InProgress", "Finished"];
+        document.getElementById("status").innerHTML = `
+            <strong>Player 1:</strong> ${p1}<br>
+            <strong>Player 2:</strong> ${p2}<br>
+            <strong>Winner:</strong> ${win}<br>
+            <strong>State:</strong> ${states[state]}
+        `;
+    } catch (error) {
+        console.error("Status update error:", error);
+    }
 }
